e38de771fa95757241a43bee76492c7366072d85
June <176018493+junapur@users.noreply.github.com>
2025-05-03 21:51:03 -0400

    chore: add noxfile

---
+++ noxfile.py
@@ -0,0 +1,46 @@
+import nox
+
+nox.options.default_venv_backend = "uv"
+nox.options.sessions = ["lint", "type_check"]
+
+
+@nox.session()
+def lint(session: nox.Session) -> None:
+    session.run_install(
+        "uv",
+        "sync",
+        "--locked",
+        "--only-group=lint",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("ruff", "check")
+    session.run("ruff", "format", "--check")
+
+
+@nox.session()
+def format(session: nox.Session) -> None:
+    session.run_install(
+        "uv",
+        "sync",
+        "--locked",
+        "--only-group=lint",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("ruff", "check", "--fix")
+    session.run("ruff", "format")
+
+
+@nox.session()
+def type_check(session: nox.Session) -> None:
+    session.run_install(
+        "uv",
+        "sync",
+        "--locked",
+        "--no-default-groups",
+        "--group",
+        "nox",
+        "--group",
+        "type-check",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("pyright")
